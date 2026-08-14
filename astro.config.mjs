fbe3ddda349ac963109a7cc8df9e5b16b4beed18
Aaronaa <116526310+AaronAAnahc@users.noreply.github.com>
2026-08-14 10:11:36 +0800

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://Aaron-AA0721.github.io/A-BLOG",
-  base: "/A-BLOG",
+  site: "https://AaronAAnahc.github.io/",
 });
